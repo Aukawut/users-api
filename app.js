84d8002300cc6b21b329a429f51233aec154ff88
aukawut2022 <110217436+aukawut2022@users.noreply.github.com>
2022-12-20 12:16:41 +0700

    chage API all

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const cors = require('cors')
 const app = express()
 const bodyParser = require('body-parser')
 const mysql = require('mysql2');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 app.use(cors())
 app.use(bodyParser.json())
-
 const connection = mysql.createConnection(process.env.DATABASE_URL);
+const secret = process.env.SECRET_KEY
+
 app.get('/users', function (req, res) {
     connection.query(
         'SELECT * FROM tb_users',
@@ -16,6 +20,57 @@ app.get('/users', function (req, res) {
             res.json({results: results})
         }
       );
+})
+app.post('/register',(req,res) => {
+    const username = req.body.username
+    const name = req.body.name
+    const password = req.body.password 
+    const phone = req.body.phone 
+    const email = req.body.email 
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+        connection.query(
+            'INSERT INTO tb_admin (username,password,name,email,phone) VALUES (?,?,?,?,?)',[username,hash,name,email,phone],
+            function(err,results){
+                if(err) {throw err }
+                res.json({err:false,results:results})
+            }
+        )
+    });
+})
+app.post('/login',(req,res) => {
+    const username = req.body.username 
+    const password = req.body.password 
+    connection.query(
+        'SELECT * FROM tb_admin WHERE username = ?',[username],
+        function(err,results){
+            if(err) { throw err }
+            if(!results.length > 0){
+            res.json({err:true,msg:'username not found'})
+
+            }else{
+                const password_hash = results[0].password
+                bcrypt.compare(password, password_hash).then(function(result) {
+                  if(result){
+                    var token = jwt.sign({ username: results[0].username,name:results[0].name },secret,{ expiresIn: '2h' });
+                    res.json({err:false,results:results,token:token})
+                  }else{
+                    res.json({err:true,msg:'password invalid'})
+                  }
+                });
+          
+        }
+        }
+    )
+})
+app.post('/authen',(req,res) => {
+    const token  = req.headers.authorization.split(' ')[1]
+    jwt.verify(token, secret, function(err, decoded) {
+        if (err) {
+            res.json({err:true,msg:'Token invalid!'})
+        }else{
+            res.json({err:false,result:decoded})
+        }
+      });
 })
 app.post('/add',(req,res) => {
     const fname = req.body.fname
